046a757f69d1e42e893db2700226ea80a55f51ee
digital.telma <digital2.telma@gmail.com>
2023-01-27 14:28:15 +0300

    responsive hse

--- js/hse.js
+++ js/hse.js
@@ -1,9 +1,7 @@
   $(function() {
-    $('.grid').masonry({
-        columnWidth: 200,
-        itemSelector: '.grid-item',
-        gutter: 10,
-        //gutter: '.gutter-sizer'
-        horizontalOrder: true
+    $('.bx-eng--list-wrapper').find('li').each(function(index, _this){
+      let a = $(_this).html(),
+          b = index + 1;
+      $(_this).html('<span>' + b + '</span>' + a);
     });
   });
